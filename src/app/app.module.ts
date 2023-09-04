b196f125eb0f78dccd0121e3ef5f27c8e9465dff
Armando Urbina <armandou@credi.mx>
2023-09-03 21:59:33 -0700

    checkpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './modules/layout/nav/nav.component';
+import { FooterComponent } from './modules/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
